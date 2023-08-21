32b4a9ac4684223b9025028a16f1f026e6150ca9
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-08-21 22:27:21 +0000

    0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,30 +1,131 @@
 #!/usr/bin/python3
-"""Import necessary modules"""
+"""Checker script for the gather_data_from_an_API.py script"""
 
 import requests
 import sys
 
-if __name__ == "__main__":
-    # Check if employee ID is provided as a command-line argument
-    if len(sys.argv) < 2:
-        print("Missing employee ID")
-        sys.exit(1)
+users_url = "https://jsonplaceholder.typicode.com/users"
+todos_url = "https://jsonplaceholder.typicode.com/todos"
 
-    # API URL and employee ID
-    URL = "https://jsonplaceholder.typicode.com"
-    EMPLOYEE_ID = sys.argv[1]
 
-    # Get employee's tasks with expanded user data
-    EMPLOYEE_TODOS = requests.get(f"{URL}/users/{EMPLOYEE_ID}/todos", params={"_expand": "user"})
-    data = EMPLOYEE_TODOS.json()
+def main_0(id):
+    """Check correct employee name"""
 
-    # Extract employee name, total tasks count, and completed tasks
-    EMPLOYEE_NAME = data[0]["user"]["name"]
-    TOTAL_NUMBER_OF_TASKS = len(data)
-    NUMBER_OF_DONE_TASKS = sum(1 for task in data if task["completed"])
+    resp = requests.get(users_url).json()
 
-    # Print employee's task progress
-    if NUMBER_OF_DONE_TASKS == TOTAL_NUMBER_OF_TASKS:
+    name = None
+    for i in resp:
+        if i['id'] == id:
+            name = i['name']
+
+    filename = 'student_output'
+
+    with open(filename, 'r') as f:
+        first = f.readline().strip()
+
+    if name in first:
+        print("Employee Name: OK")
+    else:
+        print("Employee Name: Incorrect")
+
+
+def main_1(id):
+    """Check correct number of tasks"""
+
+    todos_count = 0
+    todos_done = 0
+
+    resp = requests.get(todos_url).json()
+    for i in resp:
+        if i['userId'] == id:
+            todos_count += 1
+        if (i['completed'] and i['userId'] == id):
+            todos_done += 1
+
+    filename = 'student_output'
+    with open(filename, 'r') as f:
+        first = f.readline().strip()
+
+    if "{}/{}".format(todos_done, todos_count) in first:
         print("To Do Count: OK")
     else:
         print("To Do Count: Incorrect")
+
+
+def main_2(id):
+    """Check correct formatting of the first line"""
+
+    todos_count = 0
+    todos_done = 0
+
+    resp = requests.get(todos_url).json()
+    for i in resp:
+        if i['userId'] == id:
+            todos_count += 1
+        if (i['completed'] and i['userId'] == id):
+            todos_done += 1
+
+    resp = requests.get(users_url).json()
+
+    name = None
+    for i in resp:
+        if i['id'] == id:
+            name = i['name']
+    
+    filename = 'student_output'
+    with open(filename, 'r') as f:
+        first = f.readline().strip()
+
+    output = "Employee {} is done with tasks({}/{}):".format(name, todos_done, todos_count)
+
+    if first == output:
+        print("First line formatting: OK")
+    else:
+        print("First line formatting: Incorrect")
+
+
+def main_3(id):
+    """Check all tasks in output"""
+
+    resp = requests.get(todos_url).json()
+    tasks = [task["title"] for task in resp if task["userId"] == id and task["completed"]]
+
+    filename = 'student_output'
+    with open(filename, 'r') as f:
+        content = f.read()
+
+    for task in tasks:
+        if task not in content:
+            print(f"Task {tasks.index(task) + 1} not in output")
+
+    if all(task in content for task in tasks):
+        print("All tasks in output: OK")
+    else:
+        print("All tasks in output: Incorrect")
+
+
+def main_4():
+    """Check correct formatting of all tasks"""
+
+    resp = requests.get(todos_url).json()
+    tasks = [task["title"] for task in resp if task["completed"]]
+
+    filename = 'student_output'
+    with open(filename, 'r') as f:
+        content = f.read()
+
+    for task in tasks:
+        if "\t{}".format(task) not in content:
+            print(f"Task {tasks.index(task) + 1} Formatting: Incorrect")
+        else:
+            print(f"Task {tasks.index(task) + 1} Formatting: OK")
+
+
+if __name__ == "__main__":
+    student_id = int(sys.argv[1])
+
+    main_0(student_id)
+    main_1(student_id)
+    main_2(student_id)
+    main_3(student_id)
+    main_4()
