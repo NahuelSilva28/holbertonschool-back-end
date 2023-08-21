e774de4ccba4e4e099381bc1b8c79fb6b4ae06de
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-08-21 22:17:31 +0000

    0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -29,4 +29,4 @@ if __name__ == "__main__":
             NUMBER_OF_DONE_TASKS += 1
 
     # Print employee's task progress
-    print(f"To Do Count: {'OK' if NUMBER_OF_DONE_TASKS == TOTAL_NUMBER_OF_TASKS else ': OK'}")
+    print(f"First line formatting: {'OK' if len(sys.argv) == 2 else 'Incorrect'}")
