6a21fbd37f381b0da88f40e57f345bfc3428c372
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-08-21 23:01:38 +0000

    0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,7 +7,7 @@ import sys
 if __name__ == "__main__":
     # Check if employee ID is provided as a command-line argument
     if len(sys.argv) < 2:
-        print("Missing employee ID")
+        print("Missing employee ID as argument")
         sys.exit(1)
 
     # API URL and employee ID
@@ -21,10 +21,16 @@ if __name__ == "__main__":
     # Extract employee name, total tasks count, and completed tasks
     EMPLOYEE_NAME = data[0]["user"]["name"]
     TOTAL_NUMBER_OF_TASKS = len(data)
-    NUMBER_OF_DONE_TASKS = sum(1 for task in data if task["completed"])
+    NUMBER_OF_DONE_TASKS = 0
+    TASK_TITLE = []
+
+    # Count completed tasks and store their titles
+    for task in data:
+        if task["completed"]:
+            NUMBER_OF_DONE_TASKS += 1
+            TASK_TITLE.append(task["title"])
 
     # Print employee's task progress
-    if NUMBER_OF_DONE_TASKS == TOTAL_NUMBER_OF_TASKS:
-        print("To Do Count: OK")
-    else:
-        print("To Do Count: OK")
+    print(f"Employee {EMPLOYEE_NAME} is done with tasks ({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
+    for title in TASK_TITLE:
+        print("\t", title)
