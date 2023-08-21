f2497d0d013ceb57ca59c926e8bfedc09f5e22f6
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-08-21 22:09:12 +0000

    0-gather_data_from_an_API.py

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Import necessary modules"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    # Check if employee ID is provided as a command-line argument
+    if len(sys.argv) < 2:
+        print("Missing employee ID as argument")
+        sys.exit(1)
+
+    # API URL and employee ID
+    URL = "https://jsonplaceholder.typicode.com"
+    EMPLOYEE_ID = sys.argv[1]
+
+    # Get employee's tasks with expanded user data
+    EMPLOYEE_TODOS = requests.get(f"{URL}/users/{EMPLOYEE_ID}/todos", params={"_expand": "user"})
+    data = EMPLOYEE_TODOS.json()
+
+    # Extract employee name, total tasks count, and completed tasks
+    EMPLOYEE_NAME = data[0]["user"]["name"]
+    TOTAL_NUMBER_OF_TASKS = len(data)
+    NUMBER_OF_DONE_TASKS = 0
+    TASK_TITLE = []
+
+    # Count completed tasks and store their titles
+    for task in data:
+        if task["completed"]:
+            NUMBER_OF_DONE_TASKS += 1
+            TASK_TITLE.append(task["title"])
+
+    # Print employee's task progress
+    print(f"Employee {EMPLOYEE_NAME} is done with tasks ({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
+    for title in TASK_TITLE:
+        print("\t", title)
