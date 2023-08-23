937f33ecc9f852823bd806bd632d44d2bfd1ff8e
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-08-23 12:45:31 +0000

    1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,36 +1,31 @@
 #!/usr/bin/python3
-"""Export to CSV"""
+"""Export data to CSV format"""
 
 import csv
 import requests
-import sys
+from sys import argv
+
+def get_api():
+    """Retrieve data from an API"""
+    base_url = 'https://jsonplaceholder.typicode.com/'
+    user_id = argv[1]
+
+    # Fetch user information from API
+    user_info = requests.get(base_url + 'users/{}'.format(user_id)).json()
+
+    # Fetch tasks associated with the user ID
+    user_tasks = requests.get(base_url + 'todos', params={'userId': user_id}).json()
+
+    with open('{}.csv'.format(user_id), 'w') as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        for task in user_tasks:
+            user_id = user_id
+            username = user_info.get('username')
+            task_completed = task.get('completed')
+            task_title = task.get('title')
+
+            task_record = [user_id, username, task_completed, task_title]
+            writer.writerow(task_record)
 
 if __name__ == '__main__':
-    # Check if user ID is provided as a command-line argument
-    if len(sys.argv) != 2:
-        print("Usage: python3 {} <user_id>".format(sys.argv[0]))
-        sys.exit(1)
-
-    # Fetch user data
-    user_id = sys.argv[1]
-    user_response = requests.get
-    ('https://jsonplaceholder.typicode.com/users/{}'.format(user_id))
-    user_data = user_response.json()
-    username = user_data.get('username')
-
-    # Fetch user's tasks
-    tasks_response = requests.get
-    ('https://jsonplaceholder.typicode.com/todos?userId={}'.format(user_id))
-    tasks_data = tasks_response.json()
-
-    # Write tasks data to CSV file
-    with open('{}.csv'.format(user_id), 'w', newline='') as csv_file:
-        csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
-        csv_writer.writerow
-        (["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-
-        for task in tasks_data:
-            csv_writer.writerow
-            ([user_id, username, str(task['completed']), task['title']])
-
-    print("Data exported to {}.csv".format(user_id))
+    get_api()
